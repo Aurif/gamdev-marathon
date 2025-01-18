3ae09df500fa6c01745f5166afe05dc8804b318c
Aurif <filmosyt@gmail.com>
2025-01-18 15:11:51 +0100

    G8 - movement demo

---
+++ game/common/quarks/quark_input.gd
@@ -0,0 +1,19 @@
+class_name QuarkInput
+
+static func four_dir_movement_discrete() -> Vector2i:
+	var left = Input.get_action_strength("ui_left") > 0.2
+	var right = Input.get_action_strength("ui_right") > 0.2
+	var up = Input.get_action_strength("ui_up") > 0.2
+	var down = Input.get_action_strength("ui_down") > 0.2
+	
+	if int(left) + int(right) + int(up) + int(down) > 1:
+		return Vector2i(0, 0)
+	if left:
+		return Vector2i(-1, 0)
+	if right:
+		return Vector2i(1, 0)
+	if up:
+		return Vector2i(0, -1)
+	if down:
+		return Vector2i(0, 1)
+	return Vector2i(0, 0)
---
+++ game/game-specific/game_8/tile.gd
@@ -0,0 +1,52 @@
+extends Node
+class_name G8Tile
+
+@export var in_node: bool = false
+@export var n_player: Node2D
+@export var neighbours: Dictionary
+
+class NeighbourDefinition:
+	var node: G8Tile
+	var target_dir: Vector2i
+	
+	func _init(_node: G8Tile, _target_dir: Vector2i) -> void:
+		self.node = _node
+		self.target_dir = _target_dir
+	
+	func move_to() -> void:
+		node.enter_player(target_dir)
+
+func _ready() -> void:
+	if in_node:
+		n_player.visible = true
+		n_player.position = self.size/2
+
+func _process(delta: float) -> void:
+	if not in_node:
+		return
+	var movement = QuarkInput.four_dir_movement_discrete()
+	if movement == Vector2i(0, 0):
+		return
+		
+	if not neighbours.get(movement):
+		return
+	var next = neighbours[movement] as NeighbourDefinition
+		
+	in_node = false
+	var tween = get_tree().create_tween().bind_node(self)
+	tween.tween_property(n_player, "position", self.size/2+Vector2(movement)*self.size, 0.3).set_trans(Tween.TRANS_SINE)
+	next.move_to()
+
+func enter_player(source_dir: Vector2i) -> void:
+	n_player.visible = true
+	n_player.position = self.size/2+Vector2(source_dir)*self.size
+	var tween = get_tree().create_tween().bind_node(self)
+	tween.tween_property(n_player, "position", self.size/2, 0.3).set_trans(Tween.TRANS_SINE)
+	tween.tween_callback(_enter_player_callback)
+
+func _enter_player_callback() -> void:
+	in_node = true
+
+func register_neighbour(source_dir: Vector2i, node: G8Tile, target_dir: Vector2i) -> void:
+	assert(not neighbours.get(source_dir), "Tile had more than one neighbour registered for the same side")
+	neighbours[source_dir] = NeighbourDefinition.new(node, target_dir)
