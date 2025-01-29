8b85d46d74bd7ca4cd1b8534b59fb01478d7996b
Aurif <filmosyt@gmail.com>
2025-01-29 21:01:44 +0100

    G19 - scriptless shop

--- game/common/molecules/background_animated/background_animated.gd
+++ game/common/molecules/background_animated/background_animated.gd
@@ -12,9 +12,12 @@ func _ready() -> void:
 	
 	animation_tween = get_tree().create_tween().bind_node(self)
 	animation_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	animation_tween.tween_callback(func(): self.position = -dir*texture.get_size()*scale*MARGIN)
+	animation_tween.tween_callback(func(): self.position = -dir*texture.get_size()*_scale()*MARGIN)
 	animation_tween.tween_property(self, "position", Vector2.ZERO, speed*MARGIN)
 	animation_tween.set_loops()
 
 func _on_size_changed() -> void:
-	self.size = get_viewport().get_visible_rect().size/scale+dir*texture.get_size()*MARGIN
+	self.size = get_viewport().get_visible_rect().size/_scale()+dir*texture.get_size()*MARGIN
+
+func _scale() -> Vector2:
+	return self.get_global_transform().get_scale()
